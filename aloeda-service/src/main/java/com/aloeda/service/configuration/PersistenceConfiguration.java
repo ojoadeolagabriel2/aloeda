728de27b305d1ed0e883ac63ccf628bee848720b
Adeola <ojoadeolagabriel@gmail.com>
2020-02-17 20:21:19 +0000

    5th

---
+++ aloeda-service/src/main/java/com/aloeda/service/configuration/PersistenceConfiguration.java
@@ -0,0 +1,8 @@
+package com.aloeda.service.configuration;
+
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories
+public class PersistenceConfiguration {
+
+}
