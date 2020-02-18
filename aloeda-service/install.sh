a3fb87291061db5eae0bf63fe0d7d6377a97e5b3
Adeola <ojoadeolagabriel@gmail.com>
2020-02-18 01:41:36 +0000

    6th

--- aloeda-service/install.sh
+++ aloeda-service/install.sh
@@ -3,12 +3,18 @@
 # Author      : Adeola Ojo
 # Description : Simple app service installer
 
-# Cleanup
+# build
+./gradlew clean build
+
+# cleanup
 docker stop $(docker ps -a -q) && docker rm -f $(docker ps -a -q)
 docker image prune -f
+docker volume prune -f
+
+# switch to installer
+cd installer
 
 # Install
-./gradlew clean build
 docker-compose rm -f
 docker-compose pull
 docker-compose up --build -d
--- aloeda-service/src/main/java/com/aloeda/service/configuration/PersistenceConfiguration.java
+++ aloeda-service/src/main/java/com/aloeda/service/configuration/PersistenceConfiguration.java
@@ -4,5 +4,4 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories
 public class PersistenceConfiguration {
-
 }
