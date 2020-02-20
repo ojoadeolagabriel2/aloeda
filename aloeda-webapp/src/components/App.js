0762ae6a5d6b3064938bf382eecc3bc30d8e3702
Adeola <ojoadeolagabriel@gmail.com>
2020-02-20 20:02:11 +0000

    included basic local-installer.sh file

--- aloeda-webapp/src/components/App.js
+++ aloeda-webapp/src/components/App.js
@@ -1,33 +1,120 @@
 import React from 'react';
-import _ from "lodash"
 
-function formatWelcome(data) {
-    return `${data} and more`
+const TableBody = props => {
+    const rows = props.characterData.map((row, index) => {
+        return (
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.jobs}</td>
+                <td>
+                    <button onClick={() => props.removeCharacter(index)}>delete</button>
+                </td>
+            </tr>
+        )
+    });
+    return <tbody>{rows}</tbody>;
+};
+
+const TableHeader = () => {
+    return <thead>
+    <tr>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Action</th>
+    </tr>
+    </thead>
+};
+
+class Table extends React.Component {
+    render() {
+        const {characterData, removeCharacter} = this.props
+        return <table>
+            <TableHeader/>
+            <TableBody characterData={characterData} removeCharacter={removeCharacter}/>
+        </table>
+    }
 }
 
-class Heading extends React.Component {
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.initialState = {
+            name: '',
+            job: '',
+        };
+
+        this.state = this.initialState
+    }
+
+    handleChange = event => {
+        const {name, value} = event.target
+        this.setState({
+            [name]: value
+        })
+    };
+
+    submitForm = () => {
+        this.props.handleSubmit(this.state)
+        this.setState(this.initialState)
+    };
+
     render() {
-        const {users, message = "Hello world!"} = this.props;
-        return (<h1>{formatWelcome(message)} total users found: {_.size(users)}</h1>)
+        const {name, jobs} = this.state
+        return (
+            <form>
+                <label>Name</label>
+                <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    value={name}
+                    onChange={this.handleChange}/>
+                <label>Job</label>
+                <input
+                    type="text"
+                    name="jobs"
+                    id="jobs"
+                    value={jobs}
+                    onChange={this.handleChange}/>
+                <input type="button" value="Submit" onClick={this.submitForm}/>
+            </form>
+        )
     }
 }
 
 export default class App extends React.Component {
-    render() {
-        const users = [
+    state = {
+        characters: [
             {
-                first_name: "adeola",
-                last_name: "ojo",
+                name: "adeola",
+                jobs: "ojo",
             },
             {
-                first_name: "bolaji",
-                last_name: "ojo",
+                name: "bolaji",
+                jobs: "ojo",
             }
-        ];
+        ]
+    };
 
+    handleSubmit = character => {
+        this.setState({ characters: [...this.state.characters, character] })
+    };
+
+    removeCharacter = index => {
+        const {characters} = this.state;
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index;
+            })
+        })
+    };
+
+    render() {
         return (
-            <div className={"container"}>
-                <Heading users={users} message={"we are online"}/>
+            <div>
+                <Table characterData={this.state.characters} removeCharacter={this.removeCharacter}/>
+                <Form handleSubmit={this.handleSubmit}/>
             </div>
         )
     }
